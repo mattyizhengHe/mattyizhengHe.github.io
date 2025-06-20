434939658a9f535554854f71b2829cde2af3f6b1
mattyizhengHe <117923095+mattyizhengHe@users.noreply.github.com>
2025-06-20 20:48:41 +0000

    Deploying to gh-pages from @ mattyizhengHe/mattyizhengHe.github.io@f719eb8655c2137be5baac5515ce241261f6adae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,14 +11,14 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-resources",
           title: "Resources",
-          description: "Resources I&#39;ve used for learning, perhaps useful for you too.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resources/";
