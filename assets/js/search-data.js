c50d932bb40ace23eb8a5542a8c17e71cdd88ad3
mattyizhengHe <117923095+mattyizhengHe@users.noreply.github.com>
2025-09-21 21:18:22 +0000

    Deploying to gh-pages from @ mattyizhengHe/mattyizhengHe.github.io@e3a91c81f91144033064a2e5d9c68b753a5e70d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,6 +45,13 @@ ninja.data = [{
           window.open("https://github.com/mattyizhengHe", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/matthew-he-3bb10030b", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
