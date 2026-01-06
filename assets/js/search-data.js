528db391b3d4530267ba75cec80ca2c71c122120
mattyizhengHe <117923095+mattyizhengHe@users.noreply.github.com>
2026-01-06 15:29:02 +0000

    Deploying to gh-pages from @ mattyizhengHe/mattyizhengHe.github.io@ef6f9757f5139e22f9a41c87828b95d6a405bac6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "For full curriculum vitae with projects please send me an email.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
